3b7052329697855ce259d0bcc7ba0da932c939b0
leostan <jiaqi.hsu@gmail.com>
2016-10-06 17:22:13 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
 
-
 ## Maxtrix inversion is a time consuming computation, especially in a loop.
 ## These functions are made to compute and cache the inverse process outside the loop, 
 ## so that R can look them up later inside the loop instead of computing everytime in the loop.
@@ -9,21 +8,24 @@
 ## makeCacheMatrix function can pre-compute and cache the inverse matrix for later use.
 ## makeCacheMatrix is somehow like a multiplication table we can look up later.
 
+
+
 makeCacheMatrix <- function(x = matrix()) {
         
         inverse <- NULL
+        
         set <- function(y){
                 x <<- y
                 inverse <<- NULL
-                
         }
         
         get <- function() x
         setinverse <- function(solve) inverse <<- solve
         getinverse <- function() inverse
-        list(set=set, get=get, setinverse=setinverse, getsolve=getinverse)
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
         
 }
+
 
 
 ## cacheSolve looks up the inverse result of the “matrix” returned by makeCacheMatrix(). 
@@ -31,19 +33,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Otherwise, cacheSolve would tell makeCacheSolve to inverse the matrix, and cache its new answer for later use.
 
 
+
 cacheSolve <- function(x, ...){
         ## x here is the output of makeCacheMatrix.
-        
         inverse <- x$getinverse()
-        
         if(!is.null(inverse)){
-                ## if makeCacheMatrix has already computed.
+                ## if makeCacheMatrix has already computed the inverse matrix.
                 message("getting cache data")
                 return(inverse)
                 
         }
         
-        ## Else, tells makeCacheMatrix to inverse the matrix right now.
         data <- x$get()
         inverse <- solve(data, ...)
         x$setinverse(inverse)
